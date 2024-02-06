b0bd254853692dcb92be4acd8020ad6ef5bb630a
Sameer Ahmed <sameernadeemi1111@gmail.com>
2024-02-06 20:17:03 +0530

    Add socketio

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-
-const cors = require("cors");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+// const cors = require("cors");
 
 const app = express();
-
+const server = createServer(app);
+const io = new Server(server);
 // Enable CORS
-app.use(cors());
-app.use(express.static("build"));
+// app.use(cors());
+// app.use(express.static("build"));
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(join(__dirname, "index.html"));
 });
-
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
