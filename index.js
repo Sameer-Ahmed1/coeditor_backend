77a562f70ce0ed5dc63921427ea674af3b67c9fe
Sameer Ahmed <sameernadeemi1111@gmail.com>
2024-02-08 02:47:44 +0530

    added multifile editing

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-require("dotenv").config();
 const express = require("express");
-const { createServer } = require("node:http");
-const { join } = require("node:path");
-const { Server } = require("socket.io");
-// const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
-// Enable CORS
-// app.use(cors());
-// app.use(express.static("build"));
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
+
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("New client connected");
+  socket.on("join", (roomId) => {
+    socket.join(roomId);
+    console.log(`Client joined room ${roomId}`);
+  });
+  socket.on("leave", (roomId) => {
+    socket.leave(roomId);
+    console.log(`Client left room ${roomId}`);
+  });
+
+  socket.on("code", (roomId, newCode) => {
+    socket.to(roomId).emit("code", newCode);
+  });
+  socket.on("code", ({ roomId, fileName, newCode }) => {
+    console.log(`fileName: ${fileName}, newCode: ${newCode}`);
+    io.to(roomId).emit("code", { fileName, newCode });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+const port = process.env.PORT || 4001;
+server.listen(port, () => console.log(`Listening on port ${port}`));
